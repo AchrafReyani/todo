3f6dd435900413557224007cb8a334a852d28eac
Achraf <achrafreyani99@gmail.com>
2022-07-24 02:45:53 +0200

    add elements for todo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import TodoList from './TodoList'
 
 function App() {
-  const [todos, setTodos] = useState(["Todo1", "Todo2"]);
+  const [todos, setTodos] = useState([
+    {id: 1, name: "Todo1", completed: false },
+  ]);
+
+  const handleAddTodo = () => {
+    //タスクを追加する。
+  };
 
   return( 
     <>
     <div>
       <TodoList todos={todos}/>
       <input type="text" />
-      <button>タスクを追加</button>
+      <button onClick={handleAddTodo}>タスクを追加</button>
       <button>完了したタスクの削除</button>
       <div>残りのタスク :0
 
