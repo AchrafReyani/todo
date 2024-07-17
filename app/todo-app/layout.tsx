f491bbbe1fc4c62acf97cf2cfb4b77c96be47a31
Achraf Reyani <achrafreyani99@gmail.com>
2024-07-18 00:13:49 +0200

    added login screen with some styling

---
+++ app/todo-app/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignInComponent from '../components/signin/signin';
+
+const ToDoAppLayoutComponent = ({children} : {children: React.ReactElement}) => {
+    const isAuthenticated = false;
+  return isAuthenticated ? <div>{children}</div>
+    :<SignInComponent/>
+}
+
+export default ToDoAppLayoutComponent
---
+++ app/todo-app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToDoAppComponent = () => {
+  return (
+    <div>ToDoAppComponent</div>
+  )
+}
+
+export default ToDoAppComponent
