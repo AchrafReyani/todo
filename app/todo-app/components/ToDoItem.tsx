a3593ab192948db01645145de6247ad8e1a15501
Achraf Reyani <achrafreyani99@gmail.com>
2024-07-20 01:18:36 +0200

    small css changes to todolist

--- app/todo-app/components/ToDoItem.tsx
+++ app/todo-app/components/ToDoItem.tsx
@@ -10,6 +10,7 @@ const ToDoItemComponent = ({ todo } : {todo: {id: string, todo: string, timestam
 
     const auth = useAuth();
 
+    //these methods are all not being used anymore
     const handleCheckBox = async (e: React.ChangeEvent<HTMLInputElement>) => {
         if(!auth) return;
         let checked = e.target.checked;
@@ -33,23 +34,25 @@ const ToDoItemComponent = ({ todo } : {todo: {id: string, todo: string, timestam
         let docRef = doc(db, 'users', auth?.uid, 'todos', todo.id);
         await deleteDoc(docRef);
     }
-
+    //--------------------------
 
 
     return (
-    <div className="flex mx-1 hover:bg-slate-300">
-        <input type="checkbox" checked={todo.complete} onChange={(e) => updateStatus(auth?.uid, todo.id, e.target.checked) }/>
-        <input
-            onBlur={(e) => updateToDo(auth?.uid, todo.id, e.target.value)}
-            type="text"
-            defaultValue={todo.todo}
-            disabled={todo.complete}
-            className={
-                todo.complete ? 'border-0 ml-3 focus:border-0 focus:outline-none focus:ring-0 rounded-none line-through hover:bg-slate-300'
-                : 'border-0 ml-3 focus:border-t-0 focus:border-r-0 focus:border-l-0 focus:border-b-2 focus:outline-none focus:ring-0 rounded-none hover:bg-slate-300'
-            }
-        />
-        <button onClick={() => deleteToDo(auth?.uid,todo.id)}>x</button>
+    <div className="hover:border-2 shadow-md p-2 m-1 rounded-sm">
+        <div className="text-xs">{new Date(todo.timestamp).toDateString()}</div>
+        <div className="flex mx-1 hover:border-slate-300">
+            <input type="checkbox" checked={todo.complete} onChange={(e) => updateStatus(auth?.uid, todo.id, e.target.checked) }/>
+            <input
+                onBlur={(e) => updateToDo(auth?.uid, todo.id, e.target.value)}
+                type="text"
+                defaultValue={todo.todo}
+                disabled={todo.complete}
+                className={todo.complete ? 'complete-todo' : 'incomplete-todo'}
+            />
+            <button 
+            className="bg-red-600 text-white text-xs px-2 rounded-md"
+            onClick={() => deleteToDo(auth?.uid,todo.id)}>Delete</button>
+        </div>
     </div>
   )
 }
