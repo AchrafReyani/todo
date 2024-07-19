07b3ce0a5fb98926fd4977e04c88b7a79173c1e7
Achraf Reyani <achrafreyani99@gmail.com>
2024-07-19 14:10:14 +0200

    added todolist component still empty

---
+++ app/todo-app/components/ToDoList.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+import { db } from '@/app/lib/firebase/clientApp';
+import { collection, onSnapshot } from 'firebase/firestore';
+import useAuth from "@/app/lib/hooks/useAuth";
+
+const ToDoListComponent = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ToDoListComponent;
