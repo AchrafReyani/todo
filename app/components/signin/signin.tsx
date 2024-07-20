2b3e7b0a3fd90dc20f5c7e8c6b6bd2f202086aa1
Achraf Reyani <achrafreyani99@gmail.com>
2024-07-20 19:07:57 +0200

    proper error messages displayed when signing in/up goes wrong

--- app/components/signin/signin.tsx
+++ app/components/signin/signin.tsx
@@ -7,13 +7,15 @@ import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } f
 const SignInComponent = () => {
 
     const [ loading, setLoading ] = useState<boolean>(false);
+    const [ error, setError ] = useState<string | null>(null);
 
     const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         let email = e.currentTarget.email.value;
         let password = e.currentTarget.password.value;
-        
+
         setLoading(true);
+        setError(null);
 
         signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
@@ -22,6 +24,7 @@ const SignInComponent = () => {
         })
         .catch((error) => {
             console.log(error);
+            setError(error.message);
             setLoading(false);
         });
     }
@@ -29,61 +32,59 @@ const SignInComponent = () => {
     const handleSignUp = () => {
         let email = document.getElementsByName('email')[0] as HTMLInputElement;
         let password = document.getElementsByName('password')[0] as HTMLInputElement;
-        
+
         setLoading(true);
+        setError(null);
 
         createUserWithEmailAndPassword(auth, email.value, password.value)
         .then((userCredential) => {
-            //Signed up
+            // Signed up
             const user = userCredential.user;
             console.log(user);
             setLoading(false);
-            // ...
         })
         .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage);
+            console.log(error);
+            setError(error.message);
             setLoading(false);
-            // ...
         });
     }
 
     return (
         <div className="flex justify-center mt-24">
-        <form 
-            onSubmit={handleSignIn}
-            className="grid grid-cols-1 gap-2 w-[350px] min-w-fit items-center justify-center shadow-lg p-6 rounded-md"
-        >
-            <h1 className="text-center font-bold mb-3">Enter Credentials</h1>
-            <input 
-                className='border-2 border-slate-200 px-2 rounded-sm py-1'
-                type="email" 
-                name="email" 
-                placeholder='Email' 
-                required 
-            />
-            <input 
-                className='border-2 border-slate-200 px-2 rounded-sm py-1'
-                type="password" 
-                name="password" 
-                placeholder='Password' 
-                required
-            />
-            <button 
-                type='submit'
-                className='bg-blue-500 rounded-md text-white py-1'
-            >Log In</button>
-            <button 
-            type='button' 
-            className='bg-yellow-500 rounded-md text-white py-1'
-            onClick={handleSignUp}
-            >Sign Up</button>
-            <p className='text-center'>{ loading ? 'Signing in...' : '' }</p>
-        </form>
+            <form 
+                onSubmit={handleSignIn}
+                className="grid grid-cols-1 gap-2 w-[350px] min-w-fit items-center justify-center shadow-lg p-6 rounded-md"
+            >
+                <h1 className="text-center font-bold mb-3">Enter Credentials</h1>
+                <input 
+                    className='border-2 border-slate-200 px-2 rounded-sm py-1'
+                    type="email" 
+                    name="email" 
+                    placeholder='Email' 
+                    required 
+                />
+                <input 
+                    className='border-2 border-slate-200 px-2 rounded-sm py-1'
+                    type="password" 
+                    name="password" 
+                    placeholder='Password' 
+                    required
+                />
+                <button 
+                    type='submit'
+                    className='bg-blue-500 rounded-md text-white py-1'
+                >Log In</button>
+                <button 
+                    type='button' 
+                    className='bg-yellow-500 rounded-md text-white py-1'
+                    onClick={handleSignUp}
+                >Sign Up</button>
+                <p className='text-center'>{ loading ? 'Signing in...' : '' }</p>
+                {error && <p className='text-center text-red-500 mt-2'>{error}</p>}
+            </form>
         </div>
     )
-
 }
 
 export default SignInComponent
