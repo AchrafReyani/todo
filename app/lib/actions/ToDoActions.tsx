cbe51556313d37258192cea492ecf648e3a09558
Achraf Reyani <achrafreyani99@gmail.com>
2024-07-20 03:03:49 +0200

    credentials are stored in hidden file (#6)

--- app/lib/actions/ToDoActions.tsx
+++ app/lib/actions/ToDoActions.tsx
@@ -41,14 +41,15 @@ export const deleteToDo = async (userId: string, docId: string) => {
 
 function getApp() {
     const firebaseConfig = {
-        apiKey: "AIzaSyB-tJehHUATuAH1Ja5h_9tb4sM4nev-5IA",
-        authDomain: "todo-nextjs-f4af2.firebaseapp.com",
-        projectId: "todo-nextjs-f4af2",
-        storageBucket: "todo-nextjs-f4af2.appspot.com",
-        messagingSenderId: "592136812569",
-        appId: "1:592136812569:web:61eb67b6ac6d7f22891288",
-        measurementId: "G-RG6KZDSM04"
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
       };
+      
 
     return new Promise((resolve, reject) => {
         try {
--- app/lib/firebase/clientApp.js
+++ app/lib/firebase/clientApp.js
@@ -9,14 +9,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-tJehHUATuAH1Ja5h_9tb4sM4nev-5IA",
-  authDomain: "todo-nextjs-f4af2.firebaseapp.com",
-  projectId: "todo-nextjs-f4af2",
-  storageBucket: "todo-nextjs-f4af2.appspot.com",
-  messagingSenderId: "592136812569",
-  appId: "1:592136812569:web:61eb67b6ac6d7f22891288",
-  measurementId: "G-RG6KZDSM04"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
